b35aba80db759902b4cbd607e44afd63b543b9bf
Paulo <pauloviniciustavaressouza@gmail.com>
2022-03-09 13:13:34 -0300

    Replicando  o Facebook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import Stories from './Stories';
+import paulo_bg from './resources/IMG_6679.jpg';
+
+import { AiFillVideoCamera } from 'react-icons/ai ';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Stories />
+
+      <div className="feed">
+        <div className="feedForm">
+          <img src={paulo_bg} />
+          <input type="text" placeholder="No que você está pensando?" />
+        </div>
+        <div className="feedIcons">
+          <div className="video">
+            <AiFillVideoCamera />
+            <span>Video ao vivo</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Stories.js
@@ -0,0 +1,38 @@
+import paulo_bg from './resources/IMG_6679.jpg';
+
+export default function Stories() {
+  return (
+    <div className="stories">
+      <div
+        className="storiesCard"
+        style={{ backgroundImage: `url(${paulo_bg})` }}
+      >
+        <p>Paulo Vinicius</p>
+      </div>
+      <div
+        className="storiesCard"
+        style={{ backgroundImage: `url(${paulo_bg})` }}
+      >
+        <p>Paulo Vinicius</p>
+      </div>
+      <div
+        className="storiesCard"
+        style={{ backgroundImage: `url(${paulo_bg})` }}
+      >
+        <p>Paulo Vinicius</p>
+      </div>
+      <div
+        className="storiesCard"
+        style={{ backgroundImage: `url(${paulo_bg})` }}
+      >
+        <p>Paulo Vinicius</p>
+      </div>
+      <div
+        className="storiesCard"
+        style={{ backgroundImage: `url(${paulo_bg})` }}
+      >
+        <p>Paulo Vinicius</p>
+      </div>
+    </div>
+  );
+}
